37782a98e0ebddf4c60c083c2b0c997061751aa1
Bartłomiej Mumot <mumot1999@gmail.com>
2020-06-12 13:13:12 +0200

    add test_see_4_items_in_main_page

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,35 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Item;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        foreach (range(1, 20) as $i) {
+            factory(Item::class)->create([
+                'name' => 'zeszyt'.$i,
+                'price' => 5*100,
+                'amount' => 10,
+            ]);
+        }
+    }
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_see_4_items_in_main_page()
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
+        foreach (range(1,4) as $i) {
+            $response->assertSee('zeszyt'.$i);
+        }
     }
 }
